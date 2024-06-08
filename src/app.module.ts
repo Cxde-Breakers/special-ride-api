ae67f6ad0dead0ee626aaf9b801808d79331ee7f
Abel Nutsugah <bigmanabel@outlook.com>
2024-06-08 02:17:38 +0000

    basic auth

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { IamModule } from './iam/iam.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASS,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
