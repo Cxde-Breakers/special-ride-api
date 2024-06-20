7d2914f6252a8d9d8572b785b1ed8153c11502b1
Abel Nutsugah <bigmanabel@outlook.com>
2024-06-20 17:40:51 +0000

    added service dto

--- src/settings/dto/create-setting.dto.ts
+++ src/settings/dto/create-setting.dto.ts
@@ -1,1 +1,56 @@
-export class CreateSettingDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+
+export class CreateSettingDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    siteLogo: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    seoImage: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    favicon: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    appName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    appVersion: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    siteName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    siteTitle: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    seoMetaDescription: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    seoKeywords: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    mainMotto: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    termsAndConditions: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    privacyPolicy: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    licenses: string;
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+// import { UsersService } from './users.service';
+// import { UsersController } from './users.controller';
 import { PassengersModule } from './passengers/passengers.module';
 import { DriversModule } from './drivers/drivers.module';
 import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService],
+  // controllers: [UsersController],
+  // providers: [UsersService],
   imports: [PassengersModule, DriversModule, AdminsModule],
 })
 export class UsersModule {}
