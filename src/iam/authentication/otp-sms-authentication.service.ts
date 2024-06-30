47bb5ed24e18f59df27b2b5bb9bfe76133f14d7f
Abel Nutsugah <bigmanabel@outlook.com>
2024-06-30 00:30:46 +0000

    add error checking for verifying otp

--- src/iam/authentication/otp-sms-authentication.service.ts
+++ src/iam/authentication/otp-sms-authentication.service.ts
@@ -1,3 +1,4 @@
+import { Passenger } from './../../users/passengers/entities/passenger.entity';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Twilio } from 'twilio';
@@ -7,6 +8,7 @@ import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Passenger } from 'src/users/passengers/entities/passenger.entity';
 import { Driver } from 'src/users/drivers/entities/driver.entity';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable()
 export class OtpSmsAuthenticationService {
@@ -14,6 +16,7 @@ export class OtpSmsAuthenticationService {
 
     constructor(
         private configService: ConfigService,
+        private readonly authenticationService: AuthenticationService,
         @InjectRepository(Passenger) private readonly passengerRepository: Repository<Passenger>,
         @InjectRepository(Driver) private readonly driverRepository: Repository<Driver>,
     ) {
@@ -23,7 +26,7 @@ export class OtpSmsAuthenticationService {
         );
     }
 
-    async sendVerificationCode(otpDto: OtpDto): Promise<string> {
+    async sendVerificationCode(otpDto: OtpDto) {
         try {
             const passenger = await this.passengerRepository.findOneBy({ phoneNumber: otpDto.phoneNumber });
             const driver = await this.driverRepository.findOneBy({ phoneNumber: otpDto.phoneNumber });
@@ -46,15 +49,28 @@ export class OtpSmsAuthenticationService {
         }
     }
 
-    async verifyCode(otpDto: OtpDto): Promise<boolean> {
-        const serviceSid = this.configService.getOrThrow('TWILIO_SERVICE_SID');
-        const verificationCheck = await this.twilioClient.verify.v2.services(serviceSid)
-            .verificationChecks
-            .create({
-                to: otpDto.phoneNumber,
-                code: otpDto.code,
-            });
+    async verifyCode(otpDto: OtpDto) {
+       try {
+           const passenger = await this.passengerRepository.findOneBy({ phoneNumber: otpDto.phoneNumber });
+           const driver = await this.driverRepository.findOneBy({ phoneNumber: otpDto.phoneNumber });
 
-        return verificationCheck.status === 'approved';
+           if (!passenger && !driver) {
+               throw new BadRequestException('No user found with this phone number');
+           }
+
+           const serviceSid = this.configService.getOrThrow('TWILIO_SERVICE_SID');
+           const verificationCheck = await this.twilioClient.verify.v2.services(serviceSid)
+               .verificationChecks
+               .create({
+                   to: otpDto.phoneNumber,
+                   code: otpDto.code,
+               });
+
+           if (verificationCheck.status !== 'approved') {
+                   throw new BadRequestException('Invalid verification code');
+           }
+       } catch (error) {
+            throw new BadRequestException(error.message);
+       }
     }
 }
