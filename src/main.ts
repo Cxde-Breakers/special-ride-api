2b3ca263ec17d867522578090e7d75a75ac3c1e0
Abel Nutsugah <bigmanabel@outlook.com>
2024-06-30 10:00:02 +0000

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Special Ride API')
     .setDescription('Special Ride API for Authentication and Authorization Management and Rides Management built with NestJS')
